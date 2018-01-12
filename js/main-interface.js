e6cdcdadca5ba22583dbb698ae9b87b631d7a230
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-12 10:29:18 -0800

    get doctor first name to print to screen for search with 1 result.

--- js/main-interface.js
+++ js/main-interface.js
@@ -5,11 +5,22 @@ $(document).ready(function() {
   $('#search').submit(function() {
     event.preventDefault();
     let condition = $('#condition-search').val();
+
+    //API CALL
     getData(condition, function(response) {
-      $('#output').text(`Successful API Connection. You did it! You searched for:  ${condition}`);
-    }, function() {
-      $('#output1').text(`There was an error processing your search for ${condition} . Please try again.`);
+    //connection success
+      $('#output').append(`Successful API Connection. You did it! You searched for:  ${condition}. The search found ${response.data[0].profile}`);
+      const doctorProfile = response.data[0].profile.first_name;
+      console.log(doctorProfile);
+      $('#output2').append("Doctor Profile: " + doctorProfile);
+    },
+    //connection failure
+      function() {
+      $('#output1').text(`NO API CONNECTION. There was an error processing your search for ${condition} . Please try again.`);
     });
+    //
+
+
 
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var apiKey = require('./../.env').apiKey;
+
 export function getData(condition, arg1, arg2){
   $.ajax({
     url:`https://api.betterdoctor.com/2016-03-01/doctors?query={$condition}&location=or-portland&skip=0&limit=1&user_key=${apiKey}`,
@@ -8,6 +9,7 @@ export function getData(condition, arg1, arg2){
     },
     success: function(response) {
       arg1(response);
+
     },
     error: function(response) {
       arg2();
