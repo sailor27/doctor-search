20a65f2e2833ab30a0f29346b12354c201693ffc
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-12 10:44:18 -0800

    Update output for sucessful call and response to display number of returned results on page.

--- js/main-interface.js
+++ js/main-interface.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
     //API CALL
     getData(condition, function(response) {
     //connection success
-      $('#output').append(`Successful API Connection. You did it! You searched for:  ${condition}. The search found ${response.data[0].profile}`);
-      const doctorProfile = response.data[0].profile.first_name;
-      console.log(doctorProfile);
-      $('#output2').append("Doctor Profile: " + doctorProfile);
+      $('#output').append(`Successful API Connection. You did it! You searched for:  ${condition}. The search found ${response.data.length} results`);
+      const doctorArray = [ [response.data[0]], [response.data[1]] ];
+      console.log(doctorArray);
+      const doctor = response.data[0].profile.first_name;
+      console.log(doctor);
+      $('#output2').append("Doctor Profile: " + doctor);
     },
     //connection failure
       function() {
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 var apiKey = require('./../.env').apiKey;
-
+var searchLimit = 2;
 export function getData(condition, arg1, arg2){
   $.ajax({
-    url:`https://api.betterdoctor.com/2016-03-01/doctors?query={$condition}&location=or-portland&skip=0&limit=1&user_key=${apiKey}`,
+    url:`https://api.betterdoctor.com/2016-03-01/doctors?query={$condition}&location=or-portland&skip=0&limit=${searchLimit}&user_key=${apiKey}`,
     type: 'GET',
     data: {
       format: 'json'
     },
     success: function(response) {
       arg1(response);
-
     },
     error: function(response) {
       arg2();
