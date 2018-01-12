b0a1be4d0e4d5c07b192b32a80b56614e26c197c
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-12 09:44:40 -0800

    Separate logic for API call between front and back end.

--- js/main-interface.js
+++ js/main-interface.js
@@ -1,23 +1,15 @@
-var apiKey = require('./../.env').apiKey;
 
+import { getData } from './../js/scripts.js';
 
 $(document).ready(function() {
   $('#search').submit(function() {
     event.preventDefault();
-
     let condition = $('#condition-search').val();
-    $.ajax({
-      url:`https://api.betterdoctor.com/2016-03-01/doctors?query={$condition}&location=or-portland&skip=0&limit=1&user_key=5b888eeaf84ae50166ab683362de6b01`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('#output').text(`Successful API Connection. You did it! You searched for:  ${condition}`);
-      },
-      error: function() {
-        $('#output1').text("There was an error processing your request. Please try again.")
-      }
+    getData(condition, function(response) {
+      $('#output').text(`Successful API Connection. You did it! You searched for:  ${condition}`);
+    }, function() {
+      $('#output1').text(`There was an error processing your search for ${condition} . Please try again.`);
     });
+
   });
 });
